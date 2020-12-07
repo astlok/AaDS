12c2da2f2f0ff464272d607bf9b71c388127325d
Reutskiy Oleg <astlok@yandex.ru>
2020-12-07 15:10:49 +0300

    refact

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/astlok/Documents/IT/AaDS
-# Build directory: /Users/astlok/Documents/IT/AaDS/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
